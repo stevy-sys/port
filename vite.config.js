92fe9fd2975444fcf31f057e4c1a90cd75d0890e
stevy <stevyralambomanana@gmail.com>
2023-03-02 22:13:56 +0300

    init integration

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,10 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/sass/app.scss',
+                // 'resources/sass/app.scss',
+                'resources/css/style.css',
                 'resources/js/app.js',
+                'resources/js/main.js',
             ],
             refresh: true,
         }),
